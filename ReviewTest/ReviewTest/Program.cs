beb1dcfc5205cbe2754bd73dcab740451af3247c
Johannes Gulin <johannes.gulin@itm8.se>
2026-03-01 23:35:27 +0100

    making a loop that runs 10 times.

--- ReviewTest/ReviewTest/Program.cs
+++ ReviewTest/ReviewTest/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var nrOfPrints = 10;
+var run = true;
+
+while(run)
+{
+    Console.WriteLine("Row number = " + nrOfPrints);
+
+    nrOfPrints++;
+
+    run = nrOfPrints == 0;
+}
