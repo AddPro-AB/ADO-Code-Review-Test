a4e4cca9232793b4e26da0f23f0439a8f5641aa4
Johannes Gulin <johannes.gulin@itm8.se>
2026-03-02 11:28:18 +0100

    loop 10 times

--- ReviewTest/ReviewTest/Program.cs
+++ ReviewTest/ReviewTest/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var nrOfPrints = 10;
+var run = true;
+
+while(run)
+{
+
+    nrOfPrints++;
+
+    Console.WriteLine("Row number = " + nrOfPrints);
+
+    run = nrOfPrints > 0;
+
+} 
